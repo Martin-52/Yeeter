0c6aba6c03136124296215f60a3993f5b0a8fab8
sam laitha <samlaitha@sams-MacBook-Pro.local>
2019-12-12 15:05:34 -0800

    made profile linked to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: SigninComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../login/authentication.service';
+import { UserService } from '../service/user.service';
+import { YeetService } from '../service/yeet.service';
+import { Yeet } from '../model/yeet';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  constructor(public authenticationService: AuthenticationService, private router: Router, private userService: UserService,
+    private yeetService: YeetService) {}
+  userLoggedIn: boolean;
+  username: string;
+  userId: string;
+  yeets: Yeet[];
 
   ngOnInit() {
+    if ((localStorage.getItem('username') !== null) && (localStorage.getItem('uid') !== null)) {
+      this.userLoggedIn = true;
+      this.username = JSON.parse(localStorage.getItem('username'));
+      this.userId = JSON.parse(localStorage.getItem('uid'));
+
+      this.yeetService.followingPosts(this.userId).subscribe((res)=>{
+        this.yeets = res;
+        console.log(this.yeets);
+      });
+    }
+    
+  }
+
+  signOut() {
+    this.authenticationService.SignOut();
+    this.router.navigate(['/login']);
   }
 
 }
+
