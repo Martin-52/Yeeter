39ff5f8699d236239d696971af437e78ca20d6f8
Martin-52 <m.landin5258@gmail.com>
2019-12-11 19:00:52 -0800

    Change

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/yeeter'));
+app.use(express.static(__dirname + '/dist/Yeeter'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/yeeter/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Yeeter/index.html'));
 });
 
 // default Heroku PORT
